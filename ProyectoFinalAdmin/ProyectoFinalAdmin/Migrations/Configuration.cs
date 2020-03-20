977b018b96543c071b94b565ff2139f2a8640fba
Luis Miguel Aquino Cornelio <luism39@hotmail.com>
2020-03-20 11:07:19 -0400

    commit

---
+++ ProyectoFinalAdmin/ProyectoFinalAdmin/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace ProyectoFinalAdmin.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProyectoFinalAdmin.Models.ProyectoAdm>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProyectoFinalAdmin.Models.ProyectoAdm context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
